dff564eddfc63344d255715b2a440209c7e30136
andersonkaiti <anderkaiti@gmail.com>
2025-10-08 10:01:37 -0300

    :sparkles: feat(home): add product images and update layout for merchandise display

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,9 +1,74 @@
-import { setTimeout } from 'node:timers/promises'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const TWO_SECONDS = 2000
+export default function Home() {
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        className="group relative col-span-6 row-span-6 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
+        href="/"
+      >
+        <Image
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          height={920}
+          quality={100}
+          src="/moletom-never-stop-learning.png"
+          width={920}
+        />
 
-export default async function Home() {
-  await setTimeout(TWO_SECONDS)
+        <div className="absolute right-28 bottom-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
 
-  return <h1>Hello, World!</h1>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        className="group relative col-span-3 row-span-3 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
+        href="/"
+      >
+        <Image
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          height={920}
+          quality={100}
+          src="/moletom-java.png"
+          width={920}
+        />
+
+        <div className="absolute right-10 bottom-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
+
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        className="group relative col-span-3 row-span-3 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
+        href="/"
+      >
+        <Image
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          height={920}
+          quality={100}
+          src="/camiseta-dowhile-2022.png"
+          width={920}
+        />
+
+        <div className="absolute right-10 bottom-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
+
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$ 129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
