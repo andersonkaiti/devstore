0e497316ffed0343b7b38781ef4b3982a510257e
andersonkaiti <anderkaiti@gmail.com>
2025-10-09 08:28:01 -0300

    :sparkles: feat(http): enhance featured products API call with caching and revalidation after 1 hour

--- src/http/get-featured-products.ts
+++ src/http/get-featured-products.ts
@@ -1,8 +1,29 @@
 import { api } from './api'
 import type { IProduct } from './types/product'
 
+/**
+ * Cache & Memoization
+ * 
+ * `useMemo` / `memo` / `useCallback`
+ * 
+ * React: a memoization é feita por ele a partir do momento em que Server Components são utilizados.
+ * Isso impede que a mesma requisição seja realizada durante a mesma renderização.
+ * Vale lembrar que a mesma requisição feita em outra página não conta.
+ * 
+ * Cache: caso a requisição e os dados já estejam em mãos e, ao navegar para outra página, a mesma requisição
+ * seja realizada, mas seja preferível reutilizar as informações carregadas previamente.
+ */
+
+const SIXTY_SECONDS = 60
+const SIXTY_MINUTES = 60
+const ONE_HOUR = SIXTY_SECONDS * SIXTY_MINUTES
+
 export async function getFeaturedProducts(): Promise<IProduct[]> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: ONE_HOUR
+    }
+  })
 
   const products = await response.json()
 
