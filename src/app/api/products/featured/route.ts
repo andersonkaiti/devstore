8e7af7c0107d71397d773fa35c489165685b4f98
andersonkaiti <anderkaiti@gmail.com>
2025-10-09 08:49:37 -0300

    :sparkles: feat(home): add Skeleton component for home loading state

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,11 @@
+import { setTimeout } from 'node:timers/promises'
 import data from '../data.json' with { type: 'json' }
 
-export function GET() {
+const ONE_SECOND = 1000
+
+export async function GET() {
+  await setTimeout(ONE_SECOND)
+
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return Response.json(featuredProducts)
